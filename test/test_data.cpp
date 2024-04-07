ba60dd12f48efa9a15dfe4524b6d53ae06ad032d
gcc-bug <by.gdc@outlook.com>
2024-04-07 15:13:02 +0800

    update data

--- test/test_data.cpp
+++ test/test_data.cpp
@@ -15,7 +15,7 @@ TDD makezero(int n, dd::Package<>* ddpackage) {
     }
     return tn.cont(ddpackage);
 }
-TDD cont(dd::TensorNetwork* tn,dd::Package<>* ddpackage, int n,bool release = true) {
+TDD cont(dd::TensorNetwork* tn,dd::Package<>* ddpackage, int n,bool simulate = true,bool release = true) {
     if (!ddpackage) {
         throw std::runtime_error("ddpackage is null");
     }
@@ -24,16 +24,15 @@ TDD cont(dd::TensorNetwork* tn,dd::Package<>* ddpackage, int n,bool release = tr
     }
 
     clock_t start,end;
-    TDD res_dd = makezero(n,ddpackage);
-
-    ddpackage->incRef(res_dd.e);
-    TDD temp_dd;
-    unsigned int MAX_NODE = ddpackage->size(res_dd.e);
     start = clock();
-    for (int i=0; i < tn->tensors.size(); ++i) {
-        auto tensor = tn->tensors[i];
-        try{
-            temp_dd = ddpackage->cont(res_dd, tensor.to_tdd(ddpackage));
+    TDD res_dd = simulate ? makezero(n, ddpackage) : tn->tensors[0].to_tdd(ddpackage);
+    ddpackage->incRef(res_dd.e);
+    unsigned int MAX_NODE = ddpackage->size(res_dd.e);
+
+    // The loop starts from 0 if simulating, 1 otherwise.
+    for (size_t i = simulate ? 0 : 1; i < tn->tensors.size(); ++i) {
+        try {
+            TDD temp_dd = ddpackage->cont(res_dd, tn->tensors[i].to_tdd(ddpackage));
             if (release) {
                 ddpackage->incRef(temp_dd.e);
                 ddpackage->decRef(res_dd.e);
@@ -41,10 +40,9 @@ TDD cont(dd::TensorNetwork* tn,dd::Package<>* ddpackage, int n,bool release = tr
             }
             res_dd = temp_dd;
             MAX_NODE = std::max(MAX_NODE, ddpackage->size(res_dd.e));
-        }
-        catch(...){
+        } catch (...) {
             std::exception_ptr p = std::current_exception();
-            std::clog <<(p ? p.__cxa_exception_type()->name() : "null ") << std::endl;
+            std::clog << (p ? p.__cxa_exception_type()->name() : "null ") << std::endl;
         }
     }
     end = clock();
@@ -54,13 +52,18 @@ TDD cont(dd::TensorNetwork* tn,dd::Package<>* ddpackage, int n,bool release = tr
     return res_dd;
 };
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " <number>\n";
         return 1;
     }
+    bool simulate = true;
+    if(argc > 3){
+        simulate = false;
+    }
 
-    std::string path = std::string(PROJECT_SOURCE_DIR)+"/Benchmarks/clifford_s/";
-    std::string file_name = argv[1]+std::string("time") + ".qasm";
+
+    std::string path = std::string(PROJECT_SOURCE_DIR)+"/Benchmarks/"+argv[1]+"/";
+    std::string file_name = argv[2]+std::string("time") + ".qasm";
 	std::cout << path+file_name << std::endl;
     
     std::ifstream fileStream(path+file_name);
@@ -75,7 +78,7 @@ int main(int argc, char *argv[]) {
     auto ddPack = std::make_shared<dd::Package<>>(3*QC->getNqubits());
     auto tn = cir_2_tn(QC,ddPack);
 
-	dd::TDD tdd = cont(&tn,ddPack.get(),QC->getNqubits());
+	dd::TDD tdd = cont(&tn,ddPack.get(),QC->getNqubits(),simulate);
     
     std::cout<<"final node: " << ddPack->size(tdd.e) <<std::endl;
     return 0;
